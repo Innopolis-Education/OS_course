dd5dcecc8ee40f86ba214bd2ffe7d162f05646f7
Zener_085 <57841939+Zener085@users.noreply.github.com>
2022-09-12 16:54:35 +0300

    Update ex4.c
    Due to count must return, not prints something

--- week2/ex4.c
+++ week2/ex4.c
@@ -20,23 +20,24 @@ void lowercase(char *str, const unsigned int len) {
 /// @param str original string
 /// @param c character, which will be counted in the string
 /// @param len length of the number
-void count_char(const char *str, const char c, const unsigned int len) {
+/// @return number of exact character in the string 
+int count_char(const char *str, const char c, const unsigned int len) {
     int count = 0;
 
     for (int i = 0; i < len; i++)
         if (str[i] == c) count++;
 
-    printf("%c:%d", c, count);
+    return count;
 }
 
 /// prepare string and define length of the string before counting char
 /// @param str original string
 /// @param c character, which will be counted in the string
-void count(char *str, const char c) {
+/// @return number of exact character in the string
+int count(char *str, const char c) {
     unsigned int len = strlen(str) - 1;
     lowercase(str, len);
-    count_char(str, c, len);
-    printf("\n");
+    return count_char(str, c, len);
 }
 
 /// Counts and prints all characters from the string
@@ -45,11 +46,10 @@ void countAll(char *str) {
     unsigned int len = strlen(str) - 1;
     lowercase(str, len);
 
-    count_char(str, str[0], len);
+    printf("%c:%d", str[0], count_char(str, str[0], len));
 
     for (int i = 1; i < len; i++) {
-        printf(", ");
-        count_char(str, str[i], len);
+        printf(", %c:%d", str[i], count_char(str, str[i], len));
     }
 
     printf("\n");
