44f1cb786818db6760454c6a6ccf1f4536088337
Zener_085 <zener.085@mail.ru>
2022-11-07 21:56:35 +0300

    Week 10 exercises 1 the end

--- week10/ex1.c
+++ week10/ex1.c
@@ -7,14 +7,69 @@
 
 #define PROCESSES 1000
 #define FILE_NAME "input.txt"
+#define PAGE_BITS 8
 
 typedef struct Page {
     int number;
     unsigned int r;
 } Page;
 
-int* aging_algorithm(const int page_frames, const int references[]) {
-    return 0;
+int page_in_frame(const Page page, const Page frame[], const int page_frames) {
+    for (int i = 0; i < page_frames; i++) {
+        if (frame[i].number == page.number) return i;
+    }
+    return -1;
+}
+
+int find_min_page(const Page frame[], const int page_frames) {
+    int min_index = 0;
+    unsigned int min_r = 1000;
+    for (int i = 0; i < page_frames; i++) {
+        if (frame[i].r < min_r) {
+            min_index = i;
+            min_r = frame[i].r;
+        }
+    }
+    return min_index;
+}
+
+void decrease_r_reg(const int index, Page frame[], int page_frames) {
+    for (int i = 0; i < page_frames; i++) {
+        if (i != index) {
+            frame[i].r = frame[i].r >> 1;
+        }
+    }
+}
+
+void add_page(Page page, Page frame[], const int page_frames) {
+    int index = find_min_page(frame, page_frames);
+    frame[index] = page;
+    page.r = 0x1 << (PAGE_BITS - 1);
+    decrease_r_reg(index, frame, page_frames);
+}
+
+int aging_algorithm(const int page_frames, const Page references[]) {
+    int hit = 0;
+    Page frame[page_frames];
+    for (int i = 0; i < page_frames - PAGE_BITS; i++) {
+        frame[i] = references[i];
+    }
+    for (int i = page_frames - PAGE_BITS; i < page_frames; i++) {
+        frame[i] = references[i];
+        frame[i].r = 0x1 << (i - page_frames + PAGE_BITS);
+    }
+    int index;
+    for (int i = page_frames; i < PROCESSES; i++) {
+        index = page_in_frame(references[i], frame, page_frames);
+        if (index == -1) {
+            add_page(references[i], frame, page_frames);
+        } else {
+            hit++;
+            decrease_r_reg(-1, frame, page_frames);
+            frame[i].r += 0x1 << (PAGE_BITS - 1);
+        }
+    }
+    return hit;
 }
 
 int main(int argc, char *argv[]){
@@ -24,14 +79,15 @@ int main(int argc, char *argv[]){
 
     FILE *input = fopen(FILE_NAME, "r");
 
-    int references[PROCESSES];
+    Page references[PROCESSES];
     for (int i = 0; i < PROCESSES; i++) {
-        fscanf(input, "%d", &references[i]);
+        fscanf(input, "%d", &references[i].number);
+        references[i].r = 0;
     }
-    int *hit_miss = aging_algorithm(page_frames, references);
+    int hit = aging_algorithm(page_frames, references);
 
-    printf("Hit: %d times\n", hit_miss[0]);
-    printf("Miss: %d times\n", hit_miss[1]);
+    printf("Hit: %d times\n", hit);
+    printf("Miss: %d times\n", PROCESSES - hit);
 
     fclose(input);
     return 0;
