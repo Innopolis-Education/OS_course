e5838ad43ec4bfbb8041f87a1f24e5e78164aefd
Zener_085 <57841939+Zener085@users.noreply.github.com>
2022-11-09 16:50:21 +0300

    Week 11 exercise 1 final version

---
+++ week11/ex1.sh
@@ -0,0 +1,6 @@
+# I created files by using CLion, so there is no commands, which helps to create or print something to the files
+ln _ex1.txt _ex1_1.txt
+ln _ex1.txt _ex1_2.txt
+ls -i ex1.txt
+ls -i _ex1_1.txt
+ls -i _ex1_2.txt
